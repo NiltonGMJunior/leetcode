c090de760bef8db4ebbe0d2347abc368a77118d7
NiltonGMJunior <nilton.junior51@hotmail.com>
2019-06-17 15:51:13 -0300

    Update

---
+++ 0003_longest_substring_without_repeating_characters/main.cpp
@@ -0,0 +1,21 @@
+/* LeetCode
+ * 0003 - Longest Substring Without Repeating Characters - https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ * Nilton Gomes Martins Junior
+ * 17/06/2019
+ */
+
+#include <string>
+#include <iostream>
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(std::string s) {
+        // Naive solution (not very good O(n^2)).
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution test;
+    std::cout << test.lengthOfLongestSubstring("") << '\n';
+    return 0;
+}
